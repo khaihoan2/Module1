2628ad849afa8f77e4b2a3c9529f6136d9a8f7c5
khaihoan <ngonguyenkhaihoan@gmail.com>
2021-08-02 11:27:06 +0700

    bt

--- bt3.js
+++ bt3.js
@@ -1,16 +1,16 @@
 let vowel = ["a", "o", "e", "u", "i"];
-let countVowe = null;
+let countVowel = null;
 function countCharacters(str) {
-    countVowe = 0;
+    countVowel = 0;
     for (let i = 0; i < str.length; i++) {
         for (let j = 0; j < vowel.length; j++) {
-            if (str[1] == vowel[j]) {
+            if (str[i] == vowel[j]) {
                 countVowel++;
             }
         }
     }
     return countVowel;
 }
-let str = prompt("nhập vào chuỗi bất kỳ");
+let str = prompt("nhập vào chuỗi ký tự");
 let result = countCharacters(str);
-alert("Tổng số ký tự tìm được trong chuỗi: " + result);
+alert("Tổng số ký tự nguyên âm tìm được trong chuỗi là: " + result);
